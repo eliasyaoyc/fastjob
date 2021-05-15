ea1f8e759b34f1702ca2d981eba6b6147ddfdce9
Elias.Yao <siran0611@gmail.com>
2021-05-15 12:37:00 +0800

    add snafu crate.

--- components/utils/src/lib.rs
+++ components/utils/src/lib.rs
@@ -44,3 +44,28 @@ impl<L, R> Either<L, R> {
         }
     }
 }
+
+use snafu::{ResultExt, Snafu};
+use std::{fs, io, path::PathBuf};
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Unable to read configuration from {}: {}", path.display(), source))]
+    ReadConfiguration { source: io::Error, path: PathBuf },
+
+    #[snafu(display("Unable to write result to {}: {}", path.display(), source))]
+    WriteResult { source: io::Error, path: PathBuf },
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn process_data() -> Result<()> {
+    let path = "config.toml";
+    let configuration = fs::read_to_string(path).context(ReadConfiguration { path })?;
+    let path = unpack_config(&configuration);
+    fs::write(&path, b"My complex calculation").context(WriteResult { path })?;
+    Ok(())
+}
+
+fn unpack_config(data: &str) -> &str {
+    "/some/path/that/does/not/exist"
+}
